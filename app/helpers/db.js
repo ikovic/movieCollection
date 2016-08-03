78358cda6c3c69492ee22e06fa952655a101b5b6
Ivan <kovic.ivan@gmail.com>
2016-08-03 20:10:51 +0200

    JSCS Gulp task added

--- app/helpers/db.js
+++ app/helpers/db.js
@@ -1,26 +1,25 @@
 var MongoClient = require('mongodb').MongoClient;
-
 var state = {
-    db: null
+    db: null,
 };
 
-exports.connect = function(url, done) {
+exports.connect = function (url, done) {
     if (state.db) return done();
 
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(url, function (err, db) {
         if (err) return done(err);
         state.db = db;
         done();
     });
 };
 
-exports.get = function() {
+exports.get = function () {
     return state.db;
 };
 
-exports.close = function(done) {
+exports.close = function (done) {
     if (state.db) {
-        state.db.close(function(err, result) {
+        state.db.close(function (err, result) {
             state.db = null;
             state.mode = null;
             done(err);
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,27 @@
 // grab our packages
-var gulp   = require('gulp'),
-    jshint = require('gulp-jshint');
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    jscs = require('gulp-jscs'),
+    stylish = require('gulp-jscs-stylish');
 
 // define the default task and add the watch task to it
 gulp.task('default', ['watch']);
 
 // configure the jshint task
-gulp.task('jshint', function() {
+gulp.task('jshint', function () {
     return gulp.src('app/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
+// configure the jscs task with airbnb preset
+gulp.task('jscs', function () {
+    return gulp.src('app/**/*.js')
+        .pipe(jscs())
+        .pipe(stylish());
+});
+
 // configure which files to watch and what tasks to use on file changes
-gulp.task('watch', function() {
-    gulp.watch('app/**/*.js', ['jshint']);
+gulp.task('watch', function () {
+    gulp.watch('app/**/*.js', ['jshint', 'jscs']);
 });
