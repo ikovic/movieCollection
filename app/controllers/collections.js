9304654e0d71591d11d8118fc7b8c92822c19252
Ivan Kovic <kovic.ivan@gmail.com>
2016-08-04 13:19:55 +0200

    Upsert collections via API

--- app/controllers/collections.js
+++ app/controllers/collections.js
@@ -11,11 +11,23 @@ router.route('/collection')
         Collection.findAll(function (err, docs) {
             resHelper.handleApiResponse(err, docs, res);
         });
+    })
+    .post(function (req, res) {
+        var newCollection = req.body;
+        Collection.save(newCollection, function (err, doc) {
+            resHelper.handleApiResponse(err, doc, res);
+        });
     });
 
 router.route('/collection/:slug')
     .get(function (req, res) {
         Collection.findBySlug(req.params.slug, function (err, doc) {
+            resHelper.handleApiResponse(err, doc, res);
+        });
+    })
+    .put(function (req, res) {
+        var updatedCollection = req.body;
+        Collection.update(updatedCollection, function (err, doc) {
             resHelper.handleApiResponse(err, doc, res);
         });
     });
--- app/models/collection.js
+++ app/models/collection.js
@@ -1,4 +1,5 @@
-var db = require('../helpers/db');
+var db = require('../helpers/db'),
+    crypto = require('crypto');
 
 /**
  * Get all movie collections (one user can have only a single collection)
@@ -12,12 +13,45 @@ exports.findAll = function (cb) {
     });
 };
 
+/**
+ * Find a collection by slug
+ * @param slug
+ * @param cb
+ */
 exports.findBySlug = function (slug, cb) {
     var collection = db.get().collection('users');
 
     collection.find({slug: slug}).limit(1).next(function (err, doc) {
         cb(err, doc);
     });
+};
+
+/**
+ * Save the given collection
+ * @param newCollection
+ * @param cb
+ */
+exports.save = function (newCollection, cb) {
+    var collection = db.get().collection('users');
+
+    newCollection.slug = crypto.randomBytes(6).toString('hex');
+    collection.insertOne(newCollection, function (err, doc) {
+        cb(err, doc);
+    });
+};
+
+exports.update = function (existingCollection, cb) {
+    var collection = db.get().collection('users');
+
+    // don't set the ID
+    delete existingCollection._id;
+
+    collection.updateOne(
+        {slug: existingCollection.slug},
+        {$set: existingCollection},
+        function (err, doc) {
+            cb(err, doc);
+        });
 };
 
 // slug generator
